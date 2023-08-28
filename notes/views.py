f0e752558b2929ccd96631f32f049d5f3e70aa3c
Navneet shukla <navneetshukla824@gmail.com>
2023-08-29 00:09:13 +0530

    Designed the frontend

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Document
+
+def editor(request):
+    docid=int(request.GET.get('docid',0))
+    documents=Document.objects.all()
+    context={
+        'docid':docid,
+        'documents':documents
+    }
+    print(docid)
+    return render(request,'editor.html',context)
+    
