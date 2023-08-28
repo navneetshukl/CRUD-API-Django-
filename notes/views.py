cee0ee9efd137b0d2d12dfcf823bc06dfb0e428f
Navneet shukla <navneetshukla824@gmail.com>
2023-08-29 00:56:15 +0530

    Implemented basic functionality

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from .models import Document
@@ -6,10 +6,38 @@ from .models import Document
 def editor(request):
     docid=int(request.GET.get('docid',0))
     documents=Document.objects.all()
+    
+    if request.method=='POST':
+        docid=int(request.POST.get('docid',0))
+        title=request.POST.get('title')
+        content=request.POST.get('content','')
+        
+        if docid>0:
+            document=Document.objects.get(pk=docid)
+            document.title=title
+            document.content=content
+            document.save()
+            
+            return redirect("/?docid=%i" % docid)
+        else:
+            document=Document.objects.create(title=title, content=content)
+            
+            return redirect("/?docid=%i" % document.id)
+            
+        
+    
+    
+    if docid>0:
+        document = Document.objects.get(pk=docid)
+    else:
+        document=''
+        
     context={
         'docid':docid,
-        'documents':documents
+        'documents':documents,
+        'document':document
     }
+    
     print(docid)
     return render(request,'editor.html',context)
     
