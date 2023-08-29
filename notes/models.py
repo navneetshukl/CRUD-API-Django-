ebce131dc13377f9249f813f970cce350a44dbe0
Navneet shukla <navneetshukla824@gmail.com>
2023-08-29 11:56:10 +0530

    Completed the Project

--- notes/models.py
+++ notes/models.py
@@ -6,5 +6,8 @@ class Document(models.Model):
     title=models.CharField(max_length=255)
     content=models.TextField(blank=True,null=True)
     
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
     class Meta:
         ordering=('title',)
